87d9d4575a14c992e510e0aa2e2a2d6ace4d5558
Karma King <2114747@talnet.nl>
2020-05-14 10:44:27 +0200

    cheatsheet altijd bij te vullen met info

---
+++ 1-Syntax/taak01/index.php
@@ -0,0 +1,10 @@
+<?php 
+//cheat sheet
+// Aan het einde van een regel code gebruik je altijd een `;`
+//Ronde haakjes `()` worden gebruikt om een (voor)waarde in te zetten.
+// Accolades `{}` worden gebruikt om het begin en het einde van een codeblok aan te geven.
+//Rechte haakjes `[]` worden gebruikt om een index aan te geven
+//Variabelen beginnen altijd met een `$` gevolgd door een letter of underscore `_`
+
+
+?>
