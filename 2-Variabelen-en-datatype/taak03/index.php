19d29f4c0369e0f229ba57a7b128166ad7d2fa6d
Karma King <2114747@talnet.nl>
2020-05-19 10:10:28 +0200

    werk

---
+++ 2-Variabelen-en-datatype/taak03/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>Document</title>
+</head>
+<body>
+   <?php
+   $leeftijd = 17;
+   echo $leeftijd;
+   ?> 
+</body>
+</html>
---
+++ 2-Variabelen-en-datatype/taak04/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>Document</title>
+</head>
+<body>
+   <?php
+   $test = true;
+   echo $test;
+   ?> 
+</body>
+</html>
