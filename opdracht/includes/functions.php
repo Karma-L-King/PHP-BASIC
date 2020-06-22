8d07755a5b87d28b2745c8cc568d9925135d7c9e
Karma King <2114747@talnet.nl>
2020-06-23 00:10:33 +0200

    af :)

--- opdracht/includes/functions.php
+++ opdracht/includes/functions.php
@@ -1,22 +1,42 @@
 <?php
-$prodcutnummer = array ("peer", "appel");
+
+$aantalProd = null;
+$productnummer = null;
+$product = [
+    ["appel", 1, 0.25],
+    ["peer", 2, 0.30],
+    ["banaan",3,0.60],
+    ["kersen",4,0.64],
+    ["cola",5,1.23],
+    ["chips",6,1.51],
+    ["chocola",7,1.49],
+    ["cornflakes",8,2.12],
+    ["melk",9,1.50],
+    ["kaas",10,1.00],
+];
+
+
+
+
 
 function getInput()
 {
     $input = $_GET ['input'] ?? 'vul een waarde in hierboven en klik op [check]';
-    echo $input;
+    echo var_dump($input);
 
 }
 
 
 function getProductNr()
 {
-    $productnummer = $_GET['productnummer'] ?? 'vul een productnummer in!';
+    global $productnummer;
+    $productnummer = $_GET ['productnummer']?? 'vul een productnummer in!';
     echo $productnummer;
 }
 
 function getAmount()
 {
+    global $aantalProd;
     $aantalProd = $_GET['aantal'] ?? 'vul een aantal in!';
     echo $aantalProd;
 }
@@ -27,9 +47,38 @@ function getAge()
     echo $leeftijd;
 }
 
-function getKorting()
-{
-    
+
+
+
+function getKorting(){
+    $leeftijd= $_GET['leeftijd'];
+    switch ($leeftijd) {
+        case 19:
+            echo "0,19";
+            break;
+        case 18:
+            echo "0,18";
+            break;
+        case 17:
+            echo "0,17";
+            break;
+        case 16:
+            echo "0,16";
+            break;
+        case $leeftijd < 16:
+            echo "1,00";
+            break;
+        default:
+            echo "uw korting";
+
+    }
 }
-        
-?>
+function getTotal() {
+    global $productnummer;
+    global $product;
+    global $aantalProd;
+    echo  $product[$productnummer-1][2] * $aantalProd;
+
+}
+
+?>  
--- opdracht/producten.php
+++ opdracht/producten.php
@@ -10,32 +10,28 @@
     <form action="producten.php" method="get"><!-- dit laten staan! -->
       <?php include 'includes/menu.php'; ?>
     <p>Hieronder een lijst met producten die je bij ons kunt bestellen en de prijs per stuk:</p>
-        <table>
-            <tr>
-                <th>#</th>
-                <th>product</th>
-                <th>prijs per stuk</th>
-            </tr>
-            <tr>
-                <td>0</td>
-                <td>appels</td>
-                <td>0,25</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>peren</td>
-                <td>0,30</td>
-            </tr>
-        </table>
+    <?php
+echo "product: ". $product[0][0].", id: ".$product[0][1].", prijs/per: ".$product[0][2].".<br>";
+echo "product: ". $product[1][0].", id: ".$product[1][1].", prijs/per: ".$product[1][2].".<br>";
+echo "product: ". $product[2][0].", id: ".$product[2][1].", prijs/per: ".$product[2][2].".<br>";
+echo "product: ". $product[3][0].", id: ".$product[3][1].", prijs/per: ".$product[3][2].".<br>"; 
+echo "product: ". $product[4][0].", id: ".$product[4][1].", prijs/per: ".$product[4][2].".<br>";
+echo "product: ". $product[5][0].", id: ".$product[5][1].", prijs/per: ".$product[5][2].".<br>";
+echo "product: ". $product[6][0].", id: ".$product[6][1].", prijs/per: ".$product[6][2].".<br>";
+echo "product: ". $product[7][0].", id: ".$product[7][1].", prijs/per: ".$product[7][2].".<br>";
+echo "product: ". $product[8][0].", id: ".$product[8][1].", prijs/per: ".$product[8][2].".<br>";
+echo "product: ". $product[9][0].", id: ".$product[9][1].", prijs/per: ".$product[9][2].".<br>";
+
+?>
 
     <p>Vul hieronder het productnummer in van het product wat je wilt bestellen en de hoeveelheid.</p> 
     <p><label>productnummer: </label><input type="text" name="productnummer"> &nbsp;&nbsp;<label>aantal: </label><input type="text" name="aantal"></p><!-- dit laten staan! -->
     
     <p>Vul je leeftijd in voor korting</p>
     <p><label>leeftijd: </label><input type="text" name="leeftijd"></p><!-- dit laten staan! -->
+    <p>voor <?php getAmount(); ?> x het product: <?php getProductNr(); ?> betaal je: <?php getTotal(); ?>  </p>
+    <p>je bent <?php getAge(); ?> je korting is: <?php getKorting(); ?> </p> 
     <p><button type="submit" name="submit">Bereken totaal</button></p><!-- dit laten staan! -->
-    <p>voor <?php getAmount(); ?> x het product: <?php getProductNr(); ?> betaal je:</p>
-    <p>je bent <?php getAge(); ?> je korting is: </p>
       <?php include 'includes/footer.php'; ?>
     </form><!-- dit laten staan! -->
   </body>
